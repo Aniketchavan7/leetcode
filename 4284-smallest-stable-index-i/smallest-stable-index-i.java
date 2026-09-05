dc2356cf99ab9e90105774565e4516b8a3dcbdf7
Aniket chavan <91446947+Aniketchavan7@users.noreply.github.com>
2026-09-05 12:15:04 +0530

    Time: 0 ms (100.00%) | Memory: 46.2 MB (88.21%) - LeetSync

--- 4284-smallest-stable-index-i/smallest-stable-index-i.java
+++ 4284-smallest-stable-index-i/smallest-stable-index-i.java
@@ -3,22 +3,20 @@ class Solution {
         int n = nums.length;
         int[] suffix = new int[n];
  
-        int mn = Integer.MAX_VALUE;
-        // Build suffix minimum
-        for (int i = n - 1; i >= 0; i--) {
-            mn = Math.min(mn, nums[i]);
-            suffix[i] = mn;
+        int mn=Integer.MAX_VALUE;
+        for(int i=n-1;i>=0;i--){
+            mn=Math.min(mn,nums[i]);
+            suffix[i]=mn;
         }
- 
-        int mx = 0;
-        // Find first index with score <= k
-        for (int i = 0; i < n; i++) {
-            mx = Math.max(mx, nums[i]);
-            int score = mx - suffix[i];
-            if (score <= k)
+
+        int mx=Integer.MIN_VALUE;
+        for(int i=0;i<n;i++){
+            mx=Math.max(mx,nums[i]);
+            int s=mx-suffix[i];
+            if(s<=k){
                 return i;
+            }
         }
- 
         return -1;
     }
 }
